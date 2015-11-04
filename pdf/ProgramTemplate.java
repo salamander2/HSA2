15ded571b9330204e1c54d686a879e319d55e984
Damien Wildeve <griffin@firkin2.start.ca>
2015-11-04 11:06:31 -0500

    Add a program template to PDF directory

---
+++ pdf/ProgramTemplate.java
@@ -0,0 +1,24 @@
+package graphics1; // change this to match the name of the package (folder) that code is in
+
+/**
+ * This is a template for you to copy and use in your programs. You copy the file to a new
+ * file name, rename a few things below, and then add your code and change these comments.
+ * 
+ * @author Sam Scott, modified by M. Harwood
+ **/
+
+
+import hsa2.Console; // this imports the code from the hsa_new package
+import java.awt.Color;      // this imports code for using colors and fonts
+import java.awt.Font;
+
+public class ProgramTemplate
+{
+  static Console c = new Console(); // You can change this line to make the console bigger or smaller
+  //using ... new Console(800, 600);  or ... new Console(800, 600, "Pong Game"); 
+  
+  public static void main(String[] args)
+  {
+    // Your code goes here
+  }
+}
