f425ad87a68d25c9356f7ead174572013f2e3713
salamander2 <mk_gecko@yahoo.com>
2017-04-03 11:13:38 -0400

    update version number to 4.1

--- hsa2/ConsoleCanvas.java
+++ hsa2/ConsoleCanvas.java
@@ -42,7 +42,7 @@ import javax.swing.Timer;
  * @author Josh Gray (getRow()/getColumn() bug fix)
  * @author Tom West (old hsa code)
  * 
- * @version 4.0
+ * @version 4.1
  */
 public class ConsoleCanvas extends JPanel implements ActionListener, KeyListener {
 
