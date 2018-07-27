66872c477890bf2d84b34287db40fb6f930f5184
salamander2 <mk_gecko@yahoo.com>
2018-07-27 15:07:43 -0400

    updated Fading.java to match the PDF instructions

--- programs/first_program/Fading.java
+++ programs/first_program/Fading.java
@@ -1,8 +1,8 @@
 package graphics1;
 
-import java.awt.Color;
 
 import hsa2.GraphicsConsole;
+import java.awt.Color;
 
 public class Fading {
 
@@ -13,7 +13,7 @@ public class Fading {
 	GraphicsConsole gc = new GraphicsConsole (800,600, "Spots");
 
 	Fading(){
-		//setup
+		//Setup
 		gc.setAntiAlias(true);
 		gc.setLocationRelativeTo(null); //centre window
 //		gc.setBackgroundColor(Color.DARK_GRAY);
@@ -23,24 +23,30 @@ public class Fading {
 		
 		//variables:
 		int size = 40;
-		int sleepTime = 5;
+		int sleepTime = 5; // in milliseconds
 		
 		//main loop
 		while(true) {
 			gc.clear();
 			
-			//make random number
+			//make random numbers for location (based on 800x600)
 			int rx = (int) ((Math.random()* 700)+50);
 			int ry = (int) ((Math.random()* 500)+50);
+
 			gc.fillOval(rx,ry,size,size);
-			
+
+			/*			
 			//make random colour for next ball
 			int r = (int) (Math.random()* 256);
 			int g = (int) (Math.random()* 256);
 			int b = (int) (Math.random()* 256);
 			gc.setColor(new Color(r,g,b));
-			
-			gc.sleep(sleepTime);
+			*/
+
+			// Here’s a special way to make brighter random colours:
+			gc.setColor(new Color(Color.HSBtoRGB((float)Math.random(), 1.0f, 1.0f)));
+
+			gc.sleep(sleepTime); //you must have a sleep to allow the graphics to be updated
 		}
 	}
-}
+} //end of class
